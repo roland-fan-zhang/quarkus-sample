29ed10eb50ee54e1d87b3434374e995da752716f
Roland <roland.zhang.pro@gmail.com>
2026-05-03 21:11:11 +0200

    update Actor DTO

--- src/main/java/fr/uge/quarkus/dto/ActorResponse.java
+++ src/main/java/fr/uge/quarkus/dto/ActorResponse.java
@@ -4,13 +4,11 @@ import fr.uge.quarkus.model.Actor;
 import java.time.Instant;
 
 public record ActorResponse(
-    Short id,
     String firstName,
     String lastName,
     Instant lastUpdate
 ) {
-
   public static ActorResponse from(Actor actor) {
-    return new ActorResponse(actor.getId(), actor.getFirstName(), actor.getLastName(), actor.getLastUpdate());
+    return new ActorResponse(actor.getFirstName(), actor.getLastName(), actor.getLastUpdate());
   }
 }
